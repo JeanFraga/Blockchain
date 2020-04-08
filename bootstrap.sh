b16718aa6dbd867d9704384dd7492c216b83e56d
Jean Fraga <fragajean7@gmail.com>
2020-04-08 15:00:22 -0500

    Revert "made change to bootstrap for it to clone and start docker with flask"
    This reverts commit 545a067615434c2ffa6ea2ffc9f5d184e537c0af.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,3 @@ sudo systemctl enable docker -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-sudo groupadd docker
-sudo gpasswd -a ubuntu docker
-
-git clone --single-branch --branch Jean-Fraga https://github.com/JeanFraga/Blockchain.git
-cd Blockchain
-
-sudo docker-compose up --build
